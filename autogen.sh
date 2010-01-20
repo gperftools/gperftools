fef86cfeba8915e34575f09499e18b184d7254a9
csilvers <csilvers@6b5cf1ce-ec42-a296-1ba9-69fdba395a50>
2010-01-20 22:47:29 +0000

    * google-perftools: version 1.5 release         * Add tc_set_new_mode (willchan)         * Make memalign functions + realloc respect tc_set_new_mode (willchan)         * Add ReleaseToSystem(num_bytes) (kash)         * Handle zero-length symbols a bit better in pprof (csilvers)         * Prefer __environ to /proc/self/environ in cpu profiler (csilvers)         * Add HEAP_CHECK_MAX_LEAKS flag to control #leaks to report (glider)         * Add two new numeric pageheap properties to MallocExtension (fikes)         * Print alloc size when mmap fails (hakon)         * Add ITIMER_REAL support to cpu profiler (csilvers, nabeelmian)         * Speed up symbolizer in heap-checker reporting (glider)         * Speed up futexes with FUTEX_PRIVATE_FLAG (m3b)         * Speed up tcmalloc but doing better span coalescing (sanjay)         * Better support for different wget's and addr2maps in pprof (csilvres)         * Implement a nothrow version of delete and delete[] (csilvers)         * BUGFIX: fix a race on module_libcs[i] in windows patching (csilvers)         * BUGFIX: Fix debugallocation to call cpp_alloc for new (willchan)         * BUGFIX: A simple bugfix for --raw mode (mrabkin)         * BUGFIX: Fix C shims to actually be valid C (csilvers)         * BUGFIX: Fix recursively-unmapped-region accounting (ppluzhnikov)         * BUGFIX: better distinguish real and fake vdso (ppluzhnikov)         * WINDOWS: replace debugmodule with more reliable psai (andrey)         * PORTING: Add .bundle as another shared library extension (csilvers)         * PORTING: Fixed a typo bug in the ocnfigure PRIxx m4 macro (csilvers)         * PORTING: Augment sysinfo to work on 64-bit OS X (csilvers)         * PORTING: Use sys/ucontext.h to fix compiing on OS X 10.6 (csilvers)         * PORTING: Fix sysinfo libname reporting for solaris x86 (jeffrey)         * PORTING: Use libunwind for i386 when using --omitfp (ppluzhnikov)
    NOTE: This release uses an older version of src/windows/patch_functions.cc
    because I decided the latest optimizations were not well enough tested
    for a release.  I'll aim to get them into next release.
    
    
    git-svn-id: http://gperftools.googlecode.com/svn/trunk@84 6b5cf1ce-ec42-a296-1ba9-69fdba395a50

--- autogen.sh
+++ autogen.sh
@@ -15,11 +15,40 @@
 set -ex
 rm -rf autom4te.cache
 
-aclocal --force -I m4
-grep -q LIBTOOL configure.ac && libtoolize -c -f
+trap 'rm -f aclocal.m4.tmp' EXIT
+
+# Returns the first binary in $* that exists, or the last arg, if none exists.
+WhichOf() {
+  for candidate in "$@"; do
+    if "$candidate" --version >/dev/null 2>&1; then
+      echo "$candidate"
+      return
+    fi
+  done
+  echo "$candidate"   # the last one in $@
+}
+
+# Use version 1.9 of aclocal and automake if available.
+ACLOCAL=`WhichOf aclocal-1.9 aclocal`
+AUTOMAKE=`WhichOf automake-1.9 automake`
+LIBTOOLIZE=`WhichOf glibtoolize libtoolize15 libtoolize14 libtoolize`
+
+# aclocal tries to overwrite aclocal.m4 even if the contents haven't
+# changed, which is annoying when the file is not open for edit (in
+# p4).  We work around this by writing to a temp file and just
+# updating the timestamp if the file hasn't change.
+"$ACLOCAL" --force -I m4 --output=aclocal.m4.tmp
+if cmp aclocal.m4.tmp aclocal.m4; then
+  touch aclocal.m4               # pretend that we regenerated the file
+  rm -f aclocal.m4.tmp
+else
+  mv aclocal.m4.tmp aclocal.m4   # we did set -e above, so we die if this fails
+fi
+
+grep -q '^[^#]*AC_PROG_LIBTOOL' configure.ac && "$LIBTOOLIZE" -c -f
 autoconf -f -W all,no-obsolete
 autoheader -f -W all
-automake -a -c -f -W all,no-override
+"$AUTOMAKE" -a -c -f -W all
 
 rm -rf autom4te.cache
 exit 0
