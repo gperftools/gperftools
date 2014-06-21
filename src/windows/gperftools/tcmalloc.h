2e90b6fd72fec33aedf547d1977bdee6b77645b9
Aliaksey Kandratsenka <alk@tut.by>
2014-06-21 15:45:44 -0700

    bumped version to 2.2.1

--- src/windows/gperftools/tcmalloc.h
+++ src/windows/gperftools/tcmalloc.h
@@ -51,8 +51,8 @@
 // Define the version number so folks can check against it
 #define TC_VERSION_MAJOR  2
 #define TC_VERSION_MINOR  2
-#define TC_VERSION_PATCH  ""
-#define TC_VERSION_STRING "gperftools 2.2"
+#define TC_VERSION_PATCH  ".1"
+#define TC_VERSION_STRING "gperftools 2.2.1"
 
 #include <stdlib.h>   // for struct mallinfo, if it's defined
 
