fedceef40cd217ef406bc2522e9a8f879d60b0c0
Aliaksey Kandratsenka <alk@tut.by>
2015-09-29 11:55:35 -0700

    drop cycleclock reference in ThreadCache

--- src/thread_cache.cc
+++ src/thread_cache.cc
@@ -205,7 +205,6 @@ void ThreadCache::Scavenge() {
   // that situation by dropping L/2 nodes from the free list.  This
   // may not release much memory, but if so we will call scavenge again
   // pretty soon and the low-water marks will be high on that call.
-  //int64 start = CycleClock::Now();
   for (int cl = 0; cl < kNumClasses; cl++) {
     FreeList* list = &list_[cl];
     const int lowmark = list->lowwatermark();
