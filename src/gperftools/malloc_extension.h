caea6abf073cd593383c4fee95348b06f2789b36
Kyle Suarez <ksuarz@gmail.com>
2016-03-22 17:23:20 -0400

    Add size class statistics API
    Add an API for extracting information about central freelist size classes beyond
    what is already provided in DumpStats().
    
    A callback can be registered to extract statistics on a per-size class basis.
    The default implementation does nothing, to support implementations where there
    is no notion of size classes.

--- src/gperftools/malloc_extension.h
+++ src/gperftools/malloc_extension.h
@@ -69,6 +69,7 @@ typedef std::string MallocExtensionWriter;
 
 namespace base {
 struct MallocRange;
+struct MallocSizeClass;
 }
 
 // Interface to a pluggable system allocator.
@@ -404,6 +405,11 @@ class PERFTOOLS_DLL_DECL MallocExtension {
   // have an empty cache but will not need to pay to reconstruct the
   // cache data structures.
   virtual void MarkThreadTemporarilyIdle();
+
+  // Invokes func(arg, classinfo) for every size class.
+  // *classinfo is filled in with information about the size class.
+  typedef void (SizeClassFunction)(void*, const base::MallocSizeClass*);
+  virtual void SizeClasses(void* arg, SizeClassFunction func);
 };
 
 namespace base {
@@ -427,6 +433,17 @@ struct MallocRange {
   // - stack trace if this range was sampled
   // - heap growth stack trace if applicable to this range
   // - age when allocated (for inuse) or freed (if not in use)
+};
+
+struct MallocSizeClass {
+    uint64_t bytes_per_obj;
+    uint64_t pages_per_span;
+    uint64_t num_spans;
+    uint64_t num_thread_objs;
+    uint64_t num_central_objs;
+    uint64_t num_transfer_objs;
+    uint64_t free_bytes;
+    uint64_t alloc_bytes;
 };
 
 } // namespace base
