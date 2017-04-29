22f7ceb97a63bcbc9d824a80916bb7a809fad1f2
Aliaksey Kandratsenka <alkondratenko@gmail.com>
2017-04-29 15:25:06 -0700

    use unsigned for few flags in mini_disassembler_types.h
    We're initializing them with value that doesn't fit into signed 32-bit
    int and somehow mingw version that I have not just warns, but actually
    errors on that.

--- src/windows/mini_disassembler_types.h
+++ src/windows/mini_disassembler_types.h
@@ -188,9 +188,9 @@ struct Opcode {
   // Description of the type of the dest, src and aux operands,
   // put together from an enOperandType flag and an enAddressingMethod
   // flag.
-  int flag_dest_;
-  int flag_source_;
-  int flag_aux_;
+  unsigned flag_dest_;
+  unsigned flag_source_;
+  unsigned flag_aux_;
 
   // We indicate the mnemonic for debugging purposes
   const char* mnemonic_;
