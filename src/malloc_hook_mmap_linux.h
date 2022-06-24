f366730744bb670615f9c42dcd0453d06a68c000
Leonid Vasilev <leonid.vasilev@serokell.io>
2022-06-24 19:14:31 +0300

    Add basic support for Elbrus 2000 (e2k)
    We need to have some basic Elbrus 2000 (e2k) support because
    evil governments want to make a performance analysis for their
    evil software.

--- src/malloc_hook_mmap_linux.h
+++ src/malloc_hook_mmap_linux.h
@@ -56,7 +56,8 @@
     || defined(__aarch64__) \
     || defined(__loongarch64) \
     || (defined(_MIPS_SIM) && (_MIPS_SIM == _ABI64 || _MIPS_SIM == _ABIN32)) \
-    || defined(__s390__) || (defined(__riscv) && __riscv_xlen == 64)
+    || defined(__s390__) || (defined(__riscv) && __riscv_xlen == 64) \
+    || defined(__e2k__)
 
 static inline void* do_mmap64(void *start, size_t length,
                               int prot, int flags,
