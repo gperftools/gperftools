36fa5ee9ef55103d9edc0a206270f487f2539465
Aliaksey Kandratsenka <alkondratenko@gmail.com>
2023-11-06 13:19:17 -0500

    patch libtool.m4 for compiler_lib_search_path fix for qnx
    Amend github issue #1429

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
 autoreconf -i
+
+echo "patching m4/libtool.m4: See https://github.com/gperftools/gperftools/issues/1429#issuecomment-1794976863"
+
+(set -x; patch --forward -t --reject-file=- m4/libtool.m4 m4/libtool.patch && autoreconf -i)
