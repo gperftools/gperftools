701777bbeed26c69f649783acd40037cbae2e448
Aliaksey Kandratsenka <alkondratenko@gmail.com>
2024-03-21 18:04:39 -0400

    dont define unused HAVE_MEMORY_H in windows config.h

--- vsprojects/include/config.h
+++ vsprojects/include/config.h
@@ -92,9 +92,6 @@
 /* Define to 1 if you have the <malloc.h> header file. */
 #define HAVE_MALLOC_H 1
 
-/* Define to 1 if you have the <memory.h> header file. */
-#define HAVE_MEMORY_H 1
-
 /* Define to 1 if you have a working `mmap' system call. */
 /* #undef HAVE_MMAP */
 
