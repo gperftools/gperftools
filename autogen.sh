48cd242aadc544b2c8086247cf8ca8c4528e52a7
Aliaksei Kandratsenka <alkondratenko@gmail.com>
2025-01-12 17:05:28 -0500

    don't patch libtool anymore
    The fix is now upstream and shipping with Debian Sid.

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
 autoreconf -i
-
-echo "patching m4/libtool.m4: See https://github.com/gperftools/gperftools/issues/1429#issuecomment-1794976863"
-
-(set -x; patch --forward -t --reject-file=- m4/libtool.m4 m4/libtool.patch && autoreconf -i)
